4c7491574b3d984c00ed36a66e0fa20db4906108
SergioAle210 <ore221122@uvg.edu.gt>
2024-05-22 19:51:12 -0600

    feat(pages): create Android App with Walmart API page

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import Home from '@pages/Home/Home'
+import Calculator from '@pages/Calculator/Calculator'
+import RestaurantSystem from '@pages/RestaurantSystem/RestaurantSystem'
+import BasketballBlog from '@pages/BasketballBlog/BasketballBlog'
+import FrogGame from '@pages/FrogGame/FrogGame'
+import WalmartApp from '@pages/WalmartApp/WalmartApp'
+
 import './App.css'
 
+
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/calculator" element={<Calculator />} />
+        <Route path="/restaurantsystem" element={<RestaurantSystem />} />
+        <Route path="/basketballblog" element={<BasketballBlog />} />
+        <Route path="/froggame" element={<FrogGame />} />
+        <Route path="/walmartapp" element={<WalmartApp />} />
+      </Routes>
+    </Router>
   )
 }
 
